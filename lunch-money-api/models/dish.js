9a10a8c239cfd343316267f63a1155f5436ce912
jansedav <jansedav@oregonstate.edu>
2019-02-05 12:57:53 -0800

    Tons of changes to React

---
+++ lunch-money-api/models/dish.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Dish = mongoose.model('Dish', new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 3,
+        maxlength: 100 
+    },
+    price:{
+        type: Number,
+        min: 0,
+        max: 10000,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        max: 10000,
+        required: true
+    },
+    purchased: { 
+        type: Date, 
+        required: true
+    },
+    description: {
+        type: String,
+        required: false,
+        maxlength: 300
+    }
+}));
+
+
+function validatedish(dish) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        price: Joi.number().required(),
+        purchased: Joi.date().required(),
+        quantity: Joi.number().required(),
+        description: Joi.string()
+    };
+
+    return Joi.validate(dish, schema);
+}
+
+exports.Dish = Dish;
+exports.validatedish = validatedish;
